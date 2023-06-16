613c90632ce7e3e90c6e5a2e63a4e5b0db0b976f
mlestrella843 <marial.estrella.843@gmail.com>
2023-06-16 13:37:34 -0600

    Initializaing the project and sass configuration

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php 
+        $server="localhost";
+        $database="real_estate";
+        $user="root";
+        $password="";
+        try{
+            $connection=new PDO("mysql:host=$server;dbname=$database",$user,$password);
+         
+        }catch(Exception $error){
+            echo $error->getMessage();
+        }    
+?>
